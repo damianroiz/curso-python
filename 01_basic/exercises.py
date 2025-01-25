44d28943393aeca1d6ecec2984825fc9d593a0c0
Damian Roiz <damianx989@gmail.com>
2025-01-25 09:40:07 -0500

    first class

--- 01_basic/exercises.py
+++ 01_basic/exercises.py
@@ -3,45 +3,63 @@
 # Ejercicios para practicar los conceptos aprendidos en las lecciones.
 ###
 
-print("\nEjercicio 1: Imprimir mensajes")
-print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
+# print("\nEjercicio 1: Imprimir mensajes")
+# print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
 
-### Completa aquí
+# ### Completa aquí
 
-print("--------------")
+# nombre = input("Escribe tu nombre: ")
+# ciudad = input("Escribe tu ciudad: ")   
 
-print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
-print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
-a = 15
-b = 3.14159
-c = "Hola mundo"
-d = True
-e = None
+# print(f"Tu nombre es {nombre}\nVives en {ciudad}")
 
-### Completa aquí
+# print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+# print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+# a = 15
+# b = 3.14159
+# c = "Hola mundo"
+# d = True
+# e = None
 
-print("--------------")
+# ### Completa aquí
+# print(type(a), type(b), type(c), type(d), type(e))
 
-print("\nEjercicio 3: Casting de tipos")
-print("Convierte la cadena \"12345\" a un entero y luego a un float.")
-print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+# print("\nEjercicio 3: Casting de tipos")
+# print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+# print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
 
-### Completa aquí
+# ### Completa aquí
 
-print("--------------")
+# entero = (int("12345"))
+# print(entero)
+# float = (float(entero))
+# print(float)
 
-print("\nEjercicio 4: Variables")
-print("Crea variables para tu nombre, edad y altura.")
-print("Usa f-strings para imprimir una presentación.")
+# print(int(3.99))
 
-# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+# # 3.99 en entero se convierte en 3, se pierde la parte decimal
 
-### Completa aquí
+# print("\nEjercicio 4: Variables")
+# print("Crea variables para tu nombre, edad y altura.")
+# print("Usa f-strings para imprimir una presentación.")
 
-print("--------------")
+# # "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
 
-print("\nEjercicio 5: Números")
-print("1. Crea una variable con el número PI (sin asignar una variable)")
-print("2. Redondea el número con round()")
-print("3. Haz la división entera entre el número que te salió y el número 2")
-print("4. El resultado debería ser 1")
+# ### Completa aquí
+
+# nombre = "Damian"
+# Edad = 35
+# Altura = 1.75
+
+# print(f"Hola! Me llamo {nombre} y tengo {Edad} años, mido {Altura} metros")
+
+# print("\nEjercicio 5: Números")
+# print("1. Crea una variable con el número PI (sin asignar una variable)")
+# print("2. Redondea el número con round()")
+# print("3. Haz la división entera entre el número que te salió y el número 2")
+# print("4. El resultado debería ser 1")
+
+# PI = 3.14159
+# rounded = round(PI)
+# division = int(rounded / 2)
+# print(rounded, division)
