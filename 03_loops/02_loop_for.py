b8640906860d0cbaee5edca3e9758022d0d372b3
Damian Roiz <damianx989@gmail.com>
2025-02-02 16:52:32 -0500

    loop for completed

--- 03_loops/02_loop_for.py
+++ 03_loops/02_loop_for.py
@@ -63,24 +63,44 @@ print(pares)
 # Imprime todos los números pares del 2 al 20 (inclusive) usando un bucle for.
 print("\nEjercicio 1:")
 
+# pares = [num for num in range(2, 21) if num % 2 == 0]
+# print(pares)
+
 # Ejercicio 2: Calcular la media de una lista
 # Dada la siguiente lista de números:
-# numeros = [10, 20, 30, 40, 50]
 # Calcula la media de los números usando un bucle for.
 print("\nEjercicio 2:")
+# numeros = [10, 20, 30, 40, 50]
+# suma = 0
+
+# for num in numeros:
+#   suma += num
+# media = suma / len(numeros)
+# print(media)   
+
 
 # Ejercicio 3: Buscar el máximo de una lista
 # Dada la siguiente lista de números:
-# numeros = [15, 5, 25, 10, 20]
 # Encuentra el número máximo en la lista usando un bucle for.
 print("\nEjercicio 3:")
+numeros = [15, 5, 25, 10, 20]
+numeros.sort()
+maximo = numeros[-1]
+for num in numeros:
+  if num == maximo:
+    print(num)
+
+
 
 # Ejercicio 4: Filtrar cadenas por longitud
 # Dada la siguiente lista de palabras:
-# palabras = ["casa", "arbol", "sol", "elefante", "luna"]
 # Crea una nueva lista que contenga solo las palabras con más de 5 letras
 # usando un bucle for y list comprehension.
 print("\nEjercicio 4:")
+palabras = ["casa", "arbol", "sol", "elefante", "lunasterio"]
+palabras_largas = [palabra for palabra in palabras if len(palabra) > 5]
+print(palabras_largas)
+
 
 # Ejercicio 5: Contar palabras que empiezan con una letra
 # Dada la siguiente lista de palabras:
@@ -88,3 +108,11 @@ print("\nEjercicio 4:")
 # Pide al usuario que introduzca una letra.
 # Cuenta cuántas palabras en la lista empiezan con esa letra (sin diferenciar mayúsculas/minúsculas).
 print("\nEjercicio 5:")
+
+palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+letra = input("Introduce una letra: ").lower()
+lista = []
+for palabra in palabras:
+  if palabra.startswith(letra):
+    lista.append(palabra)
+print(f"el numero de palabras que comienzan con '{letra}' es {len(lista)}")
